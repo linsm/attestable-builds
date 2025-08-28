d32562ee064999f3029a77c3f7dbb515674a0766
Mario Lins <mario.lins@coderlime.at>
2025-08-28 11:11:25 +0200

    small fix

--- scripts/artifact-eval-setup.sh
+++ scripts/artifact-eval-setup.sh
@@ -18,7 +18,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519 -q
 
-cd attestable-builds && cargo build
+cargo build
 
 cp .env.template .env
 
